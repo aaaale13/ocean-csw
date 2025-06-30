cd1121db544ed91967803bc24ebacfe4f4e8478b
Alejo Scarola <alejto000@gmail.com>
2025-06-30 14:56:06 -0300

    Agregué el formulario de contacto con integración a MongoDB

--- app/contacto/page.tsx
+++ app/contacto/page.tsx
@@ -110,9 +110,8 @@ export default function ContactoPage() {
       <section className="py-12 md:py-24 lg:py-32">
         <div className="container px-4 md:px-6 mx-auto">
           <div className="grid gap-8 lg:grid-cols-2 lg:gap-12">
-
             {/* Contact Form */}
-            <Card className="flex flex-col space-y-6"> 
+            <Card className="flex flex-col space-y-6">
               <CardContent>
                 <ContactForm />
               </CardContent>
